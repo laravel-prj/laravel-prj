249a8e89201cca6c7920d1bc6e483d2c1ecc6aef
nqanh0110 <nguyenandie1905@gmail.com>
2021-07-01 17:22:53 +0700

    add authvalidate admin

--- laravel prj/app/Http/Controllers/admin/AuthAdmin.php
+++ laravel prj/app/Http/Controllers/admin/AuthAdmin.php
@@ -22,36 +22,21 @@ class AuthAdmin extends Controller
             'email' => $request->email,
             'password' => $request->password,
         ];
-        if ($request->remember == trans('remember.Remember Me')) {
-            $remember = true;
-        } else {
-            $remember = false;
-        }
-        //kiểm tra trường remember có được chọn hay không
-        // dd(Auth::guard('loyal_admin')->check());
 
         if (Auth::guard('loyal_admin')->attempt($arr)) {
-            // dd('đăng nhập thành công');
             return redirect('admin-mo');
-            //đăng nhập thành công thì hiển thị thông báo đăng nhập thành công
         } else {
-            dd('tài khoản và mật khẩu chưa chính xác');
-            //đăng nhập thất bại hiển thị đăng nhập thất bại
+            return redirect()->back()->withErrors("tài khoản và mật khẩu chưa chính xác")->withInput();
         }
     }
 
     public function getLogout( Request $request )
     {
-        if(Auth::guard('loyal_admin')->check()) // this means that the admin was logged in.
+        if(Auth::guard('loyal_admin')->check())
         {
             Auth::guard('loyal_admin')->logout();
             return redirect('admin-auth');
         }
-
-        // $this->guard()->logout();
-        // $request->session()->invalidate();
-
-        // return $this->loggedOut($request) ?: redirect('/');
     }
 
 
